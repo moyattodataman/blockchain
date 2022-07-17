05d29fc237b620ef03c2f6bb6216fb7d52d8ea38
モヤっとデータサイエンティスト <107286388+moyattodataman@users.noreply.github.com>
2022-07-17 10:07:25 +0900

    Add files via upload

--- 1-3/main.py
+++ 1-3/main.py
@@ -10,15 +10,15 @@ def get_transaction():
     
 @app.get("/chain")
 def get_chain():
-    # チェーンをブラウザに表示させる処理
+    # チェーンをブラウザに表示させる処理; Process to display the chain in the browser
     pass
 
 @app.post("/transaction_pool")
 def post_transaction_pool():
-    # トランザクションプールにトランザクションを追加する処理
+    # トランザクションプールにトランザクションを追加する処理; Process of adding a transaction to the transaction pool
     pass
 
 @app.get("/create_block")
 def create_block():
-    # ブロック生成処理
+    # ブロック生成処理; Block generation process
     pass
