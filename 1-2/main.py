60485ef1482e293ad11d3ed9c1d21929299f2d1e
モヤっとデータサイエンティスト <107286388+moyattodataman@users.noreply.github.com>
2022-07-17 10:06:54 +0900

    Add files via upload

--- 1-2/main.py
+++ 1-2/main.py
@@ -4,20 +4,20 @@ app = FastAPI()
 
 @app.get("/transaction_pool")
 def get_transaction():
-    # トランザクションプールをブラウザに表示させる処理
+    # トランザクションプールをブラウザに表示させる処理; Process to display the transaction pool in the browser
     pass
     
 @app.get("/chain")
 def get_chain():
-    # チェーンをブラウザに表示させる処理
+    # チェーンをブラウザに表示させる処理; Process to display the chain in the browser
     pass
 
 @app.post("/transaction_pool")
 def post_transaction_pool():
-    # トランザクションプールにトランザクションを追加する処理
+    # トランザクションプールにトランザクションを追加する処理; Process of adding a transaction to the transaction pool
     pass
 
 @app.get("/create_block")
 def create_block():
-    # ブロック生成処理
+    # ブロック生成処理; Block generation process
     pass
